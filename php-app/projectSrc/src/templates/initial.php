dbb410c0afedfad67445bd645dce184c128bfb31
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-18 14:38:15 +0800

    Initial landing page for all pages

--- php-app/projectSrc/src/templates/initial.php
+++ php-app/projectSrc/src/templates/initial.php
@@ -1,10 +1,20 @@
+<?php  ?>
+
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Welcome</title>
-</head>
-<body>
-  <h1>Hello from your first template!</h1>
-  <a href="http://localhost:8080/register">Register</a>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="/statics/bootstrap.min.css">
+    <script src="/statics/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-2 bg-light">
+    <div class="container-fluid bg-primary text-white p-3 rounded">
+      <h1>Hello from your first template!</h1>
+      <div class="container-fluid text-black bg-white rounded"> 
+        <h2>Pages List</h2>
+        <a href="http://localhost:8080/register">Register</a>
+      </div>
+    </div>
+  </body>
 </html>
