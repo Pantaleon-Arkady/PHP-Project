8e1511f9d85bae28b06b567a6d216f5429b3c22a
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-20 11:26:22 +0800

    Register Page Orientation

--- php-app/projectSrc/src/templates/register.php
+++ php-app/projectSrc/src/templates/register.php
@@ -1,7 +1,5 @@
 <?php 
 
-echo 'register page';
-
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +7,32 @@ echo 'register page';
     <head>
         <meta charset="UTF-8">
         <title>Register Page</title>
-    </head>
-    <body>
+        <link rel="stylesheet" href="/statics/bootstrap.min.css">
+        <script src="/statics/bootstrap.bundle.min.js"></script>
+        <style>
+            * {
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+            }
 
+            body {
+                height: 100vh;
+            }
+
+            .Main_div {
+                height: 100%;
+            }
+        </style>
+    </head>
+    <body class="p-3 bg-dark">
+        <div class="Main_div bg-secondary py-5 px-3 rounded d-flex flex-row">
+            <div class="bg-light w-50 m-3 rounded">
+                
+            </div>
+            <div class="bg-light w-50 m-3 rounded">
+
+            </div>
+        </div>
     </body>
 </html>
