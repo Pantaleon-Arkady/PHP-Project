73a33d16692594b0f36c5838dfc8dee330a1985e
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-24 12:59:47 +0800

    Sign Up function

--- php-app/projectSrc/src/Controllers/UsersLog.php
+++ php-app/projectSrc/src/Controllers/UsersLog.php
@@ -2,10 +2,50 @@
 
 namespace Root\Controllers;
 
+use FontLib\EOT\File;
+use Root\Database\Database;
+
 class UsersLog 
 {
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = Database::getConnection();
+    }
+
     public function Register()
     {
+
         include_once __DIR__ . '/../templates/register.php';
+
+    }
+
+    public function SignUp()
+    {
+        
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $hashed_ps = md5($password);
+
+            $registerQuery = $this->connection->prepare(
+                'INSERT INTO app_user (email, username, password) VALUES (:email, :username, :password)'
+            );
+
+            $registerQuery->execute([
+                'email' => $email,
+                'username' => $username,
+                'password' => $hashed_ps
+            ]);
+
+        }
+
+        $_SESSION['registered'] = true;
+        
+        include_once __DIR__ . '/../templates/register.php';
+        
     }
 }
