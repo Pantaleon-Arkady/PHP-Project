460041774e0cfb443a637f4d9e8bc7daf77ff1f8
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-25 13:37:36 +0800

    Log in and home page works

--- php-app/projectSrc/src/Controllers/UsersLog.php
+++ php-app/projectSrc/src/Controllers/UsersLog.php
@@ -4,6 +4,7 @@ namespace Root\Controllers;
 
 use FontLib\EOT\File;
 use Root\Database\Database;
+use \PDO;
 
 class UsersLog 
 {
@@ -51,5 +52,68 @@ class UsersLog
             $this->redirect('/register');
         }
         
+    }
+
+    public function LogIn()
+    {
+        session_start();
+        ob_start();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $namemail = $_POST['namemail'];
+            $password = $_POST['password'];
+            $hashed_ps = md5($password);
+
+            $registerQuery = $this->connection->prepare(
+                'SELECT * FROM app_user WHERE (username = :namemail OR email = :namemail) AND password = :password'
+            );
+
+            $registerQuery->execute([
+                'namemail' => $namemail,
+                'password' => $hashed_ps
+            ]);
+
+            $user = $registerQuery->fetch(PDO::FETCH_ASSOC);
+
+            echo '<pre>';
+            print_r($user);
+            echo '</pre>';
+
+            // $userId = $user['id'];
+            $_SESSION['userId'] = $user['id'];
+            $_SESSION['userInfo'] = $user;
+
+            $this->redirect('/home');
+        }
+
+        return ob_get_clean();
+
+        
+
+        // include_once __DIR__ . '/../templates/home.php';
+    }
+
+    public function Home()
+    {
+        session_start();
+        
+        $userId = $_SESSION['userId'];
+
+        $userQuery = $this->connection->prepare(
+            'SELECT * FROM app_user WHERE id = :id'
+        );
+
+        $userQuery->execute(['id' => $userId]);
+
+        $user = $userQuery->fetch(PDO::FETCH_ASSOC);
+
+        echo 'home page fetch';
+
+        echo '<pre>';
+        print_r($user);
+        echo '</pre>';
+
+        include_once __DIR__ . '/../templates/home.php';
+
     }
 }
