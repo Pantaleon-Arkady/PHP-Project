e9843505217b7c1a099cb9d1d14724093072b0a2
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-27 13:14:40 +0800

    password hashing

--- php-app/docker/database/seed.sql
+++ php-app/docker/database/seed.sql
@@ -2,6 +2,6 @@ TRUNCATE app_user RESTART IDENTITY;
 
 INSERT INTO app_user (email, username, password)
 VALUES 
-  ('first@email.com', 'first user', 'password'),
-  ('demo@demo.com', 'demo', 'demo123'),
-  ('test@test.com', 'test', 'test123');
+  ('first@email.com', 'first user', md5('password')),
+  ('demo@demo.com', 'demo', md5('demo123')),
+  ('test@test.com', 'test', md5('test123'));
