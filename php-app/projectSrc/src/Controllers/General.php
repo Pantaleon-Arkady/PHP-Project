ee7fda9d3c60ebeea8c4e262b07a83d1c9d50a33
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-07 10:04:58 +0800

    home general

--- php-app/projectSrc/src/Controllers/General.php
+++ php-app/projectSrc/src/Controllers/General.php
@@ -1,12 +1,43 @@
 <?php
 
 namespace Root\Controllers;
+
 use Root\Database\Database;
+use \PDO;
 
 class General 
 {
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = Database::getConnection();
+    }
+
     public function showWelcome()
     {
         require __DIR__ . '/../templates/initial.php';
+    }
+
+    public function userHome() {
+        session_start();
+        
+        $userId = $_SESSION['userId'];
+
+        $userQuery = $this->connection->prepare(
+            'SELECT * FROM app_user WHERE id = :id'
+        );
+
+        $userQuery->execute(['id' => $userId]);
+
+        $user = $userQuery->fetch(PDO::FETCH_ASSOC);
+
+        // echo 'home page fetch';
+
+        // echo '<pre>';
+        // print_r($user);
+        // echo '</pre>';
+
+        include_once __DIR__ . '/../templates/home.php';
     }
 }
