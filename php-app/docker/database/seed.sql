98314986be1defede0dbcac1a516723fce4dfa0b
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-09 10:52:34 +0800

    init data posts

--- php-app/docker/database/seed.sql
+++ php-app/docker/database/seed.sql
@@ -5,3 +5,22 @@ VALUES
   ('first@email.com', 'first user', md5('password')),
   ('demo@demo.com', 'demo', md5('demo123')),
   ('test@test.com', 'test', md5('test123'));
+
+INSERT INTO app_user_posts (author, title, content, created_at)
+VALUES
+  (1,
+  'Title',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+  '2025-06-04 10:45:00'),
+  (1,
+  '2nd Post',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+  '2025-07-04 10:45:00'),
+  (2,
+  'Title',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+  '2025-06-05 10:45:00'),
+  (3,
+  'Trial Title',
+  'Trial Post by Trial User',
+  '2025-07-05 10:45:00');
