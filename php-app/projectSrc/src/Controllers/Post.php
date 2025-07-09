af531f465cf4f2aae9ce55c9071311e1357f87d3
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-09 13:36:38 +0800

    all post query function

--- php-app/projectSrc/src/Controllers/Post.php
+++ php-app/projectSrc/src/Controllers/Post.php
@@ -15,5 +15,26 @@ class Post
         $this->connection = Database::getConnection();
     }
 
-    
+    public static function allPosts()
+    {
+        $allPosts = Database::fetchAll(
+            'SELECT 
+                p.id AS id,
+                p.title AS title,
+                p.content AS content,
+                p.created_at AS date_posted,
+                p.author AS author_id,
+                u.username AS author
+            FROM
+                app_user_posts p
+            LEFT JOIN
+                app_user u
+            ON
+                (p.author = u.id)
+            ORDER BY p.created_at DESC;',
+            []
+        );
+
+        return $allPosts;
+    }
 }
