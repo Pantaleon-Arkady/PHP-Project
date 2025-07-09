88472f20851313553887fadae025caa3019c2248
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-09 13:36:06 +0800

    clean all posts retrieval

--- php-app/projectSrc/src/Controllers/General.php
+++ php-app/projectSrc/src/Controllers/General.php
@@ -4,6 +4,7 @@ namespace Root\Controllers;
 
 use Root\Database\Database;
 use Root\Database\BaseTable;
+use Root\Controllers\Post;
 use \PDO;
 
 class General 
@@ -25,27 +26,12 @@ class General
         
         $userId = $_SESSION['userId'];
 
-        // $userQuery = $this->connection->prepare(
-        //     'SELECT * FROM app_user WHERE id = :id'
-        // );
-
-        // $userQuery->execute(['id' => $userId]);
-
-        // $user = $userQuery->fetch(PDO::FETCH_ASSOC);
-
         $user = Database::fetchAssoc(
             'SELECT * FROM app_user WHERE id = :id',
             ['id' => $userId]
         );
 
-        $allPosts = Database::fetchAll(
-            'SELECT * FROM app_user_posts',
-            []
-        );
-
-        echo '<pre>';
-        print_r($allPosts);
-        echo '</pre>';
+        $allPosts = Post::allPosts();
 
         include_once __DIR__ . '/../templates/home.php';
     }
