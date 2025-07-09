dce768fe0d2c753dd1eacac5017ee1a5c8d18d5f
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-09 11:05:13 +0800

    replaced query with database function

--- php-app/projectSrc/src/Controllers/General.php
+++ php-app/projectSrc/src/Controllers/General.php
@@ -25,13 +25,18 @@ class General
         
         $userId = $_SESSION['userId'];
 
-        $userQuery = $this->connection->prepare(
-            'SELECT * FROM app_user WHERE id = :id'
+        // $userQuery = $this->connection->prepare(
+        //     'SELECT * FROM app_user WHERE id = :id'
+        // );
+
+        // $userQuery->execute(['id' => $userId]);
+
+        // $user = $userQuery->fetch(PDO::FETCH_ASSOC);
+
+        $user = Database::fetchAssoc(
+            'SELECT * FROM app_user WHERE id = :id',
+            ['id' => $userId]
         );
-
-        $userQuery->execute(['id' => $userId]);
-
-        $user = $userQuery->fetch(PDO::FETCH_ASSOC);
 
         include_once __DIR__ . '/../templates/home.php';
     }
