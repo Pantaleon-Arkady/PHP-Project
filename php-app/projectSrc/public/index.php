fc46a10dd55fa8db4727cf74cad61a4b9c2181d9
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-10 15:12:57 +0800

    delete route

--- php-app/projectSrc/public/index.php
+++ php-app/projectSrc/public/index.php
@@ -35,6 +35,9 @@ switch ($uri) {
     case '/create-post':
         $post->createPost();
         break;
+    case '/delete-post':
+        $post->deletePost();
+        break;
 
 
     #General Stuff
