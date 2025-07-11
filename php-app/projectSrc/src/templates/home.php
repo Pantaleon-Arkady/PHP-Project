d29093ec603a46f9dadfbbe802ea521110f95e2b
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-11 12:45:35 +0800

    fixing product image display on card...

--- php-app/projectSrc/src/templates/home.php
+++ php-app/projectSrc/src/templates/home.php
@@ -76,7 +76,9 @@
                 <div class="col-sm-6 col-md-4 col-lg-3">
                     <a href="/product?id=1" class="text-decoration-none text-dark">
                         <div class="card product-card h-100">
-                            <img src="/files/002_boxart.png" class="card-img-top product-img" alt="Product 1">
+                            <div class="image-wrapper">
+                                <img src="/files/002_boxart.png" class="product-img" alt="Product 1">
+                            </div>
                             <div class="card-body">
                                 <h5 class="card-title">Product 1</h5>
                                 <p class="card-text text-success fw-bold">$49.99</p>
