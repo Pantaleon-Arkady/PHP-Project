94eeacecb148d86a07cd25b264380a4801dcf767
Kidd-nash <arkady.pantaleon@outlook.com>
2025-07-16 12:24:24 +0800

    is folder is writable function

--- php-app/projectSrc/src/Controllers/General.php
+++ php-app/projectSrc/src/Controllers/General.php
@@ -39,4 +39,41 @@ class General
         include_once __DIR__ . '/../templates/home.php';
     }
 
+    public static function uploadFile()
+    {
+        $uploadedFiles = [];
+
+        $uploadDir = __DIR__ . '/../../public/files/'; // Make sure this folder exists and is writable
+
+        foreach ($_FILES['fileToUpload']['name'] as $index => $fileName) {
+            if ($_FILES['fileToUpload']['error'][$index] === UPLOAD_ERR_OK) {
+                $tmpPath = $_FILES['fileToUpload']['tmp_name'][$index];
+                $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+                // Make a new safe filename
+                $newFileName = uniqid('img_', true) . '.' . $extension;
+                $destinationPath = $uploadDir . $newFileName;
+
+                if (move_uploaded_file($tmpPath, $destinationPath)) {
+                    // Store the relative URL to use later in HTML
+                    $uploadedFiles[] = '/files/' . $newFileName;
+                } else {
+                    echo "Failed to move file: $fileName";
+                }
+            } else {
+                echo "Upload error for file: $fileName";
+            }
+        }
+    }
+
+    public static function writableFolder()
+    {
+        $folder = __DIR__ . '/../../public/files';
+
+        if (is_writable($folder)) {
+            echo "Writable ✅";
+        } else {
+            echo "Not writable ❌";
+        }
+    }
 }
