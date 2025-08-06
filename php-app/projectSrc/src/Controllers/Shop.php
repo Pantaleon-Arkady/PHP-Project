6efad5e901e6c1e225ddd985ad8b56fe0b7e80a9
Kidd-nash <arkady.pantaleon@outlook.com>
2025-08-06 12:06:18 +0800

    user cart existence conditions

--- php-app/projectSrc/src/Controllers/Shop.php
+++ php-app/projectSrc/src/Controllers/Shop.php
@@ -33,8 +33,6 @@ class Shop
     {
         $productId = $_GET["id"] ?? null;
 
-        // echo $product;
-
         $product = Database::fetchAssoc(
             'SELECT * FROM app_user_products WHERE id = :id',
             [
@@ -55,29 +53,37 @@ class Shop
             ['user_id' => $userId]
         );
 
-        $cartId = $cartQuery['id'];
+        if ($cartQuery) {
 
-        $productsQuery = Database::fetchAll(
-            'SELECT
-                c.id AS product_on_cart_id,
-                c.cart_id AS cart_id,
-                c.product_id AS product_id,
-                c.quantity AS product_quantity,
-                p.name AS product_name,
-                p.price AS product_price,
-                p.image_path AS product_images
-            FROM
-                app_user_cart_products c
-            LEFT JOIN
-                app_user_products p
-            ON
-                (c.product_id = p.id)
-            WHERE
-                cart_id = :cart_id',
-            ['cart_id' => $cartId]
-        );
+            $cartId = $cartQuery['id'];
 
-        return $productsQuery;
+            $productsQuery = Database::fetchAll(
+                'SELECT
+                    c.id AS product_on_cart_id,
+                    c.cart_id AS cart_id,
+                    c.product_id AS product_id,
+                    c.quantity AS product_quantity,
+                    p.name AS product_name,
+                    p.price AS product_price,
+                    p.image_path AS product_images
+                FROM
+                    app_user_cart_products c
+                LEFT JOIN
+                    app_user_products p
+                ON
+                    (c.product_id = p.id)
+                WHERE
+                    cart_id = :cart_id',
+                ['cart_id' => $cartId]
+            );
+
+            return $productsQuery;
+
+        } else {
+
+            return [];
+
+        }
     }
 
     public function addToCart()
