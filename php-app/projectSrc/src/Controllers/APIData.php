3aee3c6058071da3d3f2a65e5c7b0693e44f483a
Kidd-nash <arkady.pantaleon@outlook.com>
2025-09-20 14:38:17 +0800

    Reminder data retrieve and upload to be fetch by React

--- php-app/projectSrc/src/Controllers/APIData.php
+++ php-app/projectSrc/src/Controllers/APIData.php
@@ -5,7 +5,8 @@ namespace Root\Controllers;
 use Root\Database\Database;
 
 class APIData {
-    public function listTasks() {
+    public function listTasks() 
+    {
         header("Access-Control-Allow-Origin: http://localhost:3000");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type");
@@ -22,6 +23,34 @@ class APIData {
             echo json_encode([
                 'success' => true,
                 'data' => $tasks
+            ]);
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'success' => false,
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function remindersList() 
+    {
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+        header("Content-Type: application/json");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit;
+        }
+
+        try {
+            $reminders = Database::fetchAll("SELECT * FROM api_reminders ORDER BY id DESC");
+
+            echo json_encode([
+                'success' => true,
+                'data' => $reminders
             ]);
         } catch (\Exception $e) {
             http_response_code(500);
