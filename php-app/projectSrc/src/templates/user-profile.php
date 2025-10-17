52cd163cbb308426d2d7bca1ae6674569eaf8235
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-17 12:43:27 +0800

    profile image width set

--- php-app/projectSrc/src/templates/user-profile.php
+++ php-app/projectSrc/src/templates/user-profile.php
@@ -4,15 +4,15 @@
 ?>
 <!-- DISABLED -->
 <?php if (1 === 5): ?>
-<?php if (isset($_SESSION['orderPlaced'])): ?>
-    <div class="alert alert-info alert-dismissible fade show d-flex justify-content-between align-items-center" role="alert">
-        <div>
-            <strong>Order Update!</strong> Your order has now been placed and is being processed for approval.
+    <?php if (isset($_SESSION['orderPlaced'])): ?>
+        <div class="alert alert-info alert-dismissible fade show d-flex justify-content-between align-items-center" role="alert">
+            <div>
+                <strong>Order Update!</strong> Your order has now been placed and is being processed for approval.
+            </div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    <?php unset($_SESSION['orderPlaced']); ?>
-<?php endif; ?>
+        <?php unset($_SESSION['orderPlaced']); ?>
+    <?php endif; ?>
 <?php endif; ?>
 
 <?php if (isset($_SESSION['orderApproved'])): ?>
@@ -26,7 +26,11 @@
 <?php endif; ?>
 <div class="container-sm border">
     <div class="row">
-        <div class="col-12 col-md-3 border">Profile Display</div>
+        <div class="col-1 col-md-3 border p-4">
+            <div class="ratio ratio-1x1">
+                <img src="image.jpg" class="rounded-circle border object-fit-cover w-100 h-100" />
+            </div>
+        </div>
         <div class="col-12 col-md-9 border">Other stuff</div>
     </div>
 </div>
